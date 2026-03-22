7d5e16163052e820c5139c2970f1fc6baf95edf8
Dawid <jarosz199806@gmail.com>
2026-03-22 19:49:52 +0100

    Add ApplicationDbContext and IApplicationDbContext

---
+++ RestaurantReservationSystem/RestaurantReservationSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservationSystem.Domain.Entities;
+using System.Reflection;
+
+namespace RestaurantReservationSystem.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<Admin> Admins { get; set; }
+    public DbSet<Guest> Guests { get; set; }
+    public DbSet<GuestEmail> GuestEmails { get; set; }
+    public DbSet<GuestPhone> GuestPhones { get; set; }
+    public DbSet<Permission> Permissions { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<Restaurant> Restaurants { get; set; }
+    public DbSet<RestaurantHours> RestaurantsHours { get; set; }
+    public DbSet<RestaurantTable> RestaurantTables { get; set; }
+    public DbSet<Staff> Staff { get; set; }
+    public DbSet<StaffPermission> StaffPermissions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
+
---
+++ RestaurantReservationSystem/RestaurantReservationSystem.Infrastructure/Data/IApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservationSystem.Domain.Entities;
+
+namespace RestaurantReservationSystem.Infrastructure.Data;
+
+public interface IApplicationDbContext
+{
+    DbSet<Admin> Admins { get; }
+    DbSet<Guest> Guests { get; }
+    DbSet<GuestEmail> GuestEmails { get; }
+    DbSet<GuestPhone> GuestPhones { get; }
+    DbSet<Permission> Permissions { get; }
+    DbSet<RefreshToken> RefreshTokens { get; }
+    DbSet<Reservation> Reservations { get; }
+    DbSet<Restaurant> Restaurants { get; }
+    DbSet<RestaurantHours> RestaurantsHours { get; }
+    DbSet<RestaurantTable> RestaurantTables { get; }
+    DbSet<Staff> Staff { get; }
+    DbSet<StaffPermission> StaffPermissions { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
