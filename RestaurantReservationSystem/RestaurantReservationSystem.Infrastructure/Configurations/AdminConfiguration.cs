73e803ab932c919e4a767fc06540f542639005c6
Dawid <jarosz199806@gmail.com>
2026-03-22 21:02:17 +0100

    Add first configuration template file

---
+++ RestaurantReservationSystem/RestaurantReservationSystem.Infrastructure/Configurations/AdminConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RestaurantReservationSystem.Domain.Entities;
+
+namespace RestaurantReservationSystem.Infrastructure.Configurations;
+
+public class AdminConfiguration : IEntityTypeConfiguration<Admin>
+{
+    public void Configure(EntityTypeBuilder<Admin> builder)
+    {
+        builder.HasKey(a => a.Id);
+
+        builder.Property(a => a.Login)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(a => a.Email)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(a => a.PasswordHash)
+            .IsRequired();
+
+        builder.HasOne(a => a.Restaurant)
+            .WithOne(r => r.Admin)
+            .HasForeignKey<Restaurant>(r => r.AdminId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(a => a.RefreshTokens)
+            .WithOne(rt => rt.Admin)
+            .HasForeignKey(rt => rt.AdminId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
+
